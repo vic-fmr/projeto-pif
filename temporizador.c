e7ab635bf28d25e377adffb247a41d05956f2d0c
Lucas Rodrigues <luccxzs@gmail.com>
2025-05-18 20:25:49 -0300

    Delete temporizador.c

--- temporizador.c
+++
@@ -1,9 +0,0 @@
-#include "temporizador.h"
-
-void iniciar_temporizador(time_t *inicio) {
-    *inicio = time(NULL);
-}
-
-double parar_temporizador(time_t inicio) {
-    return difftime(time(NULL), inicio);
-}
