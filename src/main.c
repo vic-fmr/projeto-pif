3238469e1020b5cd939c3c340cdb25c309c226f4
renysoo <rene_lucena@hotmail.com>
2025-05-24 16:08:22 -0300

    Tela inicial

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@
 #include "screen.h"
 #include "keyboard.h"
 #include "timer.h"
+#include "ui.h"
 
 int x = 34, y = 12;
 int incX = 1, incY = 1;
@@ -46,44 +47,7 @@ void printKey(int ch)
 
 int main() 
 {
-    static int ch = 0;
-    static long timer = 0;
-
-    screenInit(1);
-    keyboardInit();
-    timerInit(50);
-
-    printHello(x, y);
-    screenUpdate();
-
-    while (ch != 10 && timer <= 100) //enter or 5s
-    {
-        // Handle user input
-        if (keyhit()) 
-        {
-            ch = readch();
-            printKey(ch);
-            screenUpdate();
-        }
-
-        // Update game state (move elements, verify collision, etc)
-        if (timerTimeOver() == 1)
-        {
-            int newX = x + incX;
-            if (newX >= (MAXX -strlen("Hello World") -1) || newX <= MINX+1) incX = -incX;
-            int newY = y + incY;
-            if (newY >= MAXY-1 || newY <= MINY+1) incY = -incY;
-
-            printHello(newX, newY);
-
-            screenUpdate();
-            timer++;
-        }
-    }
-
-    keyboardDestroy();
-    screenDestroy();
-    timerDestroy();
+    tela_inicial();
 
     return 0;
 }
